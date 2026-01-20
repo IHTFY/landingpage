49175d6d809569df3866c566906efe6042e5a65d
Frankie <13478274+IHTFY@users.noreply.github.com>
2026-01-19 19:25:50 -0800

    remove arach since links are temporary

--- script.js
+++
@@ -1,9 +0,0 @@
-const uploadURL = atob(
-  "aHR0cHM6Ly96aXBsaW5lLmZyYW5raWVtZXJjYWRvLmNvbS9nby91cGxvYWQ=",
-);
-const viewURL = atob(
-  "aHR0cHM6Ly9pbW1pY2guZnJhbmtpZW1lcmNhZG8uY29tL3Mvd2VkZGluZw==",
-);
-
-document.getElementById("uploadBtn").href = uploadURL;
-document.getElementById("viewBtn").href = viewURL;
